aa6329174ed801e82a07673ee60639af2e52eed6
ElenaKilljoy <lenokcardio@gmail.com>
2023-02-09 11:03:42 +0300

    feat: add current date in momentum

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,5 +1,5 @@
 const time = document.querySelector('.time');
-
+const dateToday = document.querySelector('.date');
 
 function showTime() {
   const date = new Date();
@@ -9,4 +9,12 @@ setTimeout(showTime, 1000);
 }
 showTime();
 
+function showDate() {
+  const date = new Date();
+  const options = {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'};
+  const currentDate = date.toLocaleDateString('en-RU', options);
+  dateToday.textContent = currentDate;
+  setTimeout(showDate, 1000);
+}
+showDate();
 
